07b09a9a34c2f3274ff1cb87e8cda383a626a583
chabuuuu <haphuthinh332004@gmail.com>
2024-03-23 19:20:56 +0700

    first init

---
+++ src/database/db.datasource.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+const path = require('path');
+const entities_path = path.join(__dirname, '..','models','*')
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: process.env.DB_HOST || "localhost",
+    port: Number(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USERNAME || "postgres",
+    password: process.env.DB_PASSWORD || "admin",
+    database: process.env.DB_NAME || "test",
+    entities: [entities_path],
+    synchronize: true,
+    logging: false,
+    migrations: [__dirname + "/migrations/*.js"],
+})

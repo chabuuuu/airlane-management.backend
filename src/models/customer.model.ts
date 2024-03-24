a78b630b7d91594dba134ec3c0d49331c932f456
p-kTmm <tamnvk2004@gmail.com>
2024-03-24 14:00:28 +0700

    Thêm file customer.model.ts

---
+++ src/models/customer.model.ts
@@ -0,0 +1,43 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Customer {
+    @PrimaryGeneratedColumn("uuid")
+    customerId!: string;
+
+    @Column("varchar", { length: 50, unique: true })
+    email!: string;
+
+    @Column("varchar", { length: 15, unique: true })
+    phoneNumber!: string;
+
+    @Column("varchar", { length: 30 })
+    password!: string;
+
+    @Column("date")
+    birthday!: Date;
+
+    @Column("varchar", { length: 50 })
+    address!: string;
+
+    @Column("varchar", { length: 50 })
+    nationality!: string;
+
+    @Column({ type: "boolean", default: false })
+    emailValidated!: boolean;
+
+    @Column("varchar", { length: 50, unique: true })
+    cccd!: string;
+
+    @Column("text", { unique: true })
+    cccdPicture!: string;
+
+    @Column("text", { nullable: true })
+    profilePicture!: string | null;
+
+    @CreateDateColumn()
+    createAt!: Date;
+
+    @UpdateDateColumn()
+    updateAt!: Date;
+}

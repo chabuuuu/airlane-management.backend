59bb108f18f0048f2185c06bc96bb82e990e2fa6
chabuuuu <haphuthinh332004@gmail.com>
2024-04-02 08:31:06 +0700

    build(swagger) delete account endpoint

--- src/routes/account/account.route.ts
+++ src/routes/account/account.route.ts
@@ -4,26 +4,6 @@ import { classValidate } from "@/middleware/class-validate.middleware";
 import express from "express";
 const accountRouter = express.Router();
 accountRouter
-
-/**
- * @swagger
- * /account/create:
- *   post:
- *     summary: Create a account user.
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: "#/components/schemas/CreateAccountDto"
- *     responses:
- *       200:
- *         description: The account was successfully created.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: "#/components/schemas/CreateAccount200Response"
-*/
   .post(
     "/create",
     classValidate(CreateAccountDto),

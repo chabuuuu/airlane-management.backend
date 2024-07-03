2ee52fda5fc80c6732d70c761bcf24649054ee80
chabuuuu <haphuthinh332004@gmail.com>
2024-07-03 23:05:37 +0700

    update(model: ticket + booking): add passengerinfo

--- src/models/booking.model.ts
+++ src/models/booking.model.ts
@@ -1,66 +1,92 @@
-import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { Ticket } from '@/models/ticket.model';
-import { BookingStatus } from '@/enums/booking-status.enum';
-import { Customer } from '@/models/customer.model';
-import moment from 'moment-timezone';
-import { SeatFlight } from '@/models/seat_flight.model.';
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Ticket } from "@/models/ticket.model";
+import { BookingStatus } from "@/enums/booking-status.enum";
+import { Customer } from "@/models/customer.model";
+import moment from "moment-timezone";
+import { SeatFlight } from "@/models/seat_flight.model.";
 
 @Entity()
 export class Booking {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn("uuid")
   bookingId!: string;
 
   // @Column({nullable: true})
   // ticketId!: string;
 
-  @Column({ type: 'boolean', default: false })
+  @Column({ type: "boolean", default: false })
   paymentStatus!: boolean;
 
-  @Column({ type: 'enum', enum: BookingStatus, default: BookingStatus.BOOKED})
+  @Column({ type: "enum", enum: BookingStatus, default: BookingStatus.BOOKED })
   bookingStatus!: string;
 
+  //This is the account that create this booking
   @Column()
   passengerId!: string;
 
-  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  //Passenger info-------------------
+  @Column({ nullable: true })
+  full_name?: string;
+
+  @Column({ nullable: true })
+  phone_number?: string;
+
+  @Column({ nullable: true })
+  email?: string;
+
+  @Column({ nullable: true })
+  cccd?: string;
+  //--------------------------------
+
+  @Column({ type: "decimal", precision: 10, scale: 2 })
   price!: number;
 
-  @CreateDateColumn({ type: 'datetime', 
-  transformer: {
-    to: (value: Date) => value,
-    from: (value: string) => {
-        const raw = moment(value)
-        const vn = raw.clone().tz('Asia/Ho_Chi_Minh');
+  @CreateDateColumn({
+    type: "datetime",
+    transformer: {
+      to: (value: Date) => value,
+      from: (value: string) => {
+        const raw = moment(value);
+        const vn = raw.clone().tz("Asia/Ho_Chi_Minh");
         return vn.format("DD-MM-YYYY HH:mm:ss");
-    }
-}
-   })
+      },
+    },
+  })
   bookedAt!: Date;
 
-  @UpdateDateColumn({ type: 'datetime', 
-  transformer: {
-    to: (value: Date) => value,
-    from: (value: string) => {
-        const raw = moment(value)
-        const vn = raw.clone().tz('Asia/Ho_Chi_Minh');
+  @UpdateDateColumn({
+    type: "datetime",
+    transformer: {
+      to: (value: Date) => value,
+      from: (value: string) => {
+        const raw = moment(value);
+        const vn = raw.clone().tz("Asia/Ho_Chi_Minh");
         return vn.format("DD-MM-YYYY HH:mm:ss");
-    }
-}
-   })
+      },
+    },
+  })
   updateAt!: Date;
 
   // //FKs:
   // @OneToOne(() => Ticket)
   // @JoinColumn({ name: 'ticketId' })
-  // ticket!: Ticket;  
+  // ticket!: Ticket;
 
-  @ManyToOne(() => Customer, (passenger) => passenger.bookings) 
-  @JoinColumn({ name: 'passengerId' })
+  @ManyToOne(() => Customer, (passenger) => passenger.bookings)
+  @JoinColumn({ name: "passengerId" })
   passenger!: Customer;
 
-  @OneToOne(()=>SeatFlight, (seatFlight) => seatFlight.booking ,
-{
-  "cascade":  ["update"]
-})
+  @OneToOne(() => SeatFlight, (seatFlight) => seatFlight.booking, {
+    cascade: ["update"],
+  })
   seatFlight!: SeatFlight;
 }
--- src/models/ticket.model.ts
+++ src/models/ticket.model.ts
@@ -1,68 +1,95 @@
-import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn, PrimaryGeneratedColumn, OneToOne } from 'typeorm';
-import { Flight } from '@/models/flight.model';
-import { Staff } from '@/models/staff.model';
-import { Customer } from '@/models/customer.model';
-import { TicketStatus } from '@/enums/ticket-status.enum';
-import moment from 'moment-timezone';
-import { SeatFlight } from '@/models/seat_flight.model.';
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+  PrimaryGeneratedColumn,
+  OneToOne,
+} from "typeorm";
+import { Flight } from "@/models/flight.model";
+import { Staff } from "@/models/staff.model";
+import { Customer } from "@/models/customer.model";
+import { TicketStatus } from "@/enums/ticket-status.enum";
+import moment from "moment-timezone";
+import { SeatFlight } from "@/models/seat_flight.model.";
 
 @Entity()
 export class Ticket {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn("uuid")
   ticketId!: string;
 
   @Column()
-  flightId!: string
+  flightId!: string;
 
   @Column()
   passengerId!: string;
 
-  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  //Passenger info-------------------
+  @Column({ nullable: true })
+  full_name?: string;
+
+  @Column({ nullable: true })
+  phone_number?: string;
+
+  @Column({ nullable: true })
+  email?: string;
+
+  @Column({ nullable: true })
+  cccd?: string;
+  //--------------------------------
+
+  @Column({ type: "decimal", precision: 10, scale: 2 })
   price!: number;
 
   @Column()
-  sellerId!: string
+  sellerId!: string;
 
-  @Column({ type: 'enum', enum: TicketStatus, default: TicketStatus.Pending })
+  @Column({ type: "enum", enum: TicketStatus, default: TicketStatus.Pending })
   status!: string;
 
   @CreateDateColumn({
-  transformer: {
-    to: (value: Date) => value,
-    from: (value: string) => {
-        const raw = moment(value)
-        const vn = raw.clone().tz('Asia/Ho_Chi_Minh');
+    transformer: {
+      to: (value: Date) => value,
+      from: (value: string) => {
+        const raw = moment(value);
+        const vn = raw.clone().tz("Asia/Ho_Chi_Minh");
         return vn.format("DD-MM-YYYY HH:mm:ss");
-    }
-}
-   })
+      },
+    },
+  })
   sellAt!: Date;
 
-  @UpdateDateColumn({ type: 'datetime',     transformer: {
-    to: (value: Date) => value,
-    from: (value: string) => {
-        const raw = moment(value)
-        const vn = raw.clone().tz('Asia/Ho_Chi_Minh');
+  @UpdateDateColumn({
+    type: "datetime",
+    transformer: {
+      to: (value: Date) => value,
+      from: (value: string) => {
+        const raw = moment(value);
+        const vn = raw.clone().tz("Asia/Ho_Chi_Minh");
         return vn.format("DD-MM-YYYY HH:mm:ss");
-    }
-} })
+      },
+    },
+  })
   updateAt!: Date;
 
   //FKs:
-  @ManyToOne(() => Customer, (passenger) => passenger.tickets) 
-  @JoinColumn({ name: 'passengerId' })
+  @ManyToOne(() => Customer, (passenger) => passenger.tickets)
+  @JoinColumn({ name: "passengerId" })
   passenger!: Customer;
 
   @ManyToOne(() => Staff, (staff) => staff.tickets)
-  @JoinColumn({name: 'sellerId'})
+  @JoinColumn({ name: "sellerId" })
   seller!: Staff;
 
   @ManyToOne(() => Flight, (flight) => flight.tickets)
-  @JoinColumn({name: 'flightId'})
+  @JoinColumn({ name: "flightId" })
   flight!: Flight;
 
-  @OneToOne(()=>SeatFlight, (seatFlight) => seatFlight.ticket, 
-{cascade: ["update"]})
+  @OneToOne(() => SeatFlight, (seatFlight) => seatFlight.ticket, {
+    cascade: ["update"],
+  })
   seatFlight!: SeatFlight;
 }
-
